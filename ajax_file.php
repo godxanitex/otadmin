4ce117d78326ab1ac31d2751df79efd713c50c92
godxanitex <xanitex@tntopentibia.com>
2013-03-29 12:14:31 -0500

    Fixed major security vulnerability that allowed any file to be displayed via ajax_file.php

--- ajax_file.php
+++ ajax_file.php
@@ -1,22 +1,4 @@
 <?php
-    /*
-    Copyright (C) 2013 - God Xanitex
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    */
-?>
-<?php
     session_start();
     include "config.php";
     if (!isset($_SESSION['authed']) or $_SESSION['authed'] == false)
@@ -25,5 +7,13 @@
         die();
     }
     $name = $_GET['name'];
-    echo htmlentities(file_get_contents($name));
+    $log_path = $ot_root . '/data/logs';
+    if (strstr($name,$log_path) and !strstr($name,'..') or strstr($name,'dumps') and !strstr($name,'..'))
+    {
+        echo htmlentities(file_get_contents($name));
+    }
+    else
+    {
+        echo 'You are not able to access this file.';
+    }
 ?>
